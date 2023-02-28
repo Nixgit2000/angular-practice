8ad1575a23be3396d1260b9af9a685783f2ce8bb
Nixgit2000 <fourwaycoordination@gmail.com>
2023-02-28 11:21:22 -0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { TemplateComponent } from './template/template.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    TemplateComponent,
+
   ],
   imports: [
     BrowserModule
